a61f661525fb7bc8a1292efccc27e7e9a91b15d0
EoneRizuwan <mhmmdrzwn31@gmail.com>
2018-12-27 14:48:39 +0800

    Change gitignore and update Main class

---
+++ main/java/com/anubiscode/gittest/Main.java
@@ -0,0 +1,9 @@
+package main.java.com.anubiscode.gittest;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Counting from 1 to 10");
+		for (int i = 1; i <= 10; i++) System.out.println(i);
+	}
+}
